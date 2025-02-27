1d40703a5e36b8100d87f1918f0156180b762414
Andy Kim <bkim71@student.gsu.edu>
2025-02-27 13:39:02 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const { body, validationResult } = require("express-validator");
 const admin = require("firebase-admin");
 const path = require("path");
 
-const serviceAccount = require(path.join(__dirname, "serviceAccountKey.json"));
+const serviceAccount = require(path.join(__dirname, "serviceaccountKey.json"));
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
